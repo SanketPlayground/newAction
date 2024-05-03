9137a6ba7d9e1df5b3c09bf370f45d117c223ee3
Sanket Desai <sanketde@canarys.com>
2024-05-03 13:08:01 +0530

    5th commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-
+var fetch = require('node-fetch');
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
@@ -10,6 +10,44 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
+
+
+try {
+    var organization = core.getInput('organization');
+    var accessToken = core.getInput('token');
+    
+    function fetchRepositories() {
+      try {
+        fetch(`https://api.github.com/orgs/${organization}/repos`, {
+          headers: {
+            Authorization: `token ${accessToken}`,
+          },
+        })
+        .then(function(response) {
+          if (!response.ok) {
+            throw new Error(`Failed to fetch repositories: ${response.status} ${response.statusText}`);
+          }
+          return response.json();
+        })
+        .then(function(repositories) {
+          console.log("Repositories in the organization:");
+          repositories.forEach(function(repo) {
+            console.log(repo.name);
+          });
+        })
+        .catch(function(error) {
+          console.error('Error:', error.message);
+        });
+      } catch (error) {
+        core.setFailed(error.message);
+      }
+    }
+    
+    fetchRepositories();
+    
 } catch (error) {
   core.setFailed(error.message);
 }
