1e62188360bb03c334835d2da128c51deca9dbd7
Sanket Desai <sanketde@canarys.com>
2024-05-07 18:14:54 +0530

    36th

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const fs = require('fs');
-const path = require('path');
-const archiver = require('archiver');
-const xlsx = require('xlsx');
 const core = require('@actions/core');
 const axios = require('axios');
 
@@ -31,50 +27,11 @@ async function getSecretScanningAlerts(org, repo, token) {
             }
         });
 
-        return response.data.map(alert => [
-            alert.html_url || '',
-            alert.secret_type || '',
-            alert.secret || '',
-            alert.state || '',
-            alert.resolution || ''
-        ]);
+        return response.data;
     } catch (error) {
         console.error('Failed to fetch secret scanning alerts:', error);
         throw error;
     }
-}
-
-async function generateExcel(data, org, repo) {
-    const wb = xlsx.utils.book_new();
-    const ws = xlsx.utils.aoa_to_sheet(data);
-    const sheetName = `${repo.substring(0, 30)}-secret-scanning-alerts`;
-    xlsx.utils.book_append_sheet(wb, ws, sheetName);
-    const outputPath = path.join(process.env.GITHUB_WORKSPACE, `${repo}-alerts.xlsx`); // Save in GitHub workspace directory
-    xlsx.writeFile(wb, outputPath);
-    return outputPath;
-}
-
-
-async function createZip(org, token) {
-    const repos = await getAllRepos(org, token);
-    const zipName = `${org}-secret-scanning-alerts.zip`;
-    const output = fs.createWriteStream(zipName);
-    const archive = archiver('zip', { zlib: { level: 9 } });
-    archive.pipe(output);
-
-    for (const repo of repos) {
-        try {
-            const alerts = await getSecretScanningAlerts(org, repo, token);
-            const excelPath = await generateExcel(alerts, org, repo);
-            archive.file(excelPath, { name: `${repo}-alerts.xlsx` });
-            fs.unlinkSync(excelPath); // remove the generated Excel file after adding to zip
-        } catch (error) {
-            console.error('Failed to process repo:', repo, error);
-        }
-    }
-
-    archive.finalize();
-    return zipName;
 }
 
 async function run() {
@@ -84,8 +41,16 @@ async function run() {
         if (!org || !token) {
             throw new Error('Organization or token is not provided.');
         }
-        const zipFilePath = await createZip(org, token);
-        core.setOutput('zip-file', zipFilePath);
+
+        const repos = await getAllRepos(org, token);
+        for (const repo of repos) {
+            try {
+                // const alerts = await getSecretScanningAlerts(org, repo, token);
+                console.log(`Secret scanning alerts for ${repo}:`, "alerts");
+            } catch (error) {
+                console.error('Failed to process repo:', repo, error);
+            }
+        }
     } catch (error) {
         core.setFailed(error.message);
     }
